8dd905977184f750ae962129fd9b4e0394e38711
Peter Olanipekun <olanipekunp@gmail.com>
2026-03-31 12:03:28 +0000

    split Hermite-Hadamard into two goals

--- analysis.lean
+++ analysis.lean
@@ -1,4 +1,4 @@
-import Mathlib.Analysis.Convex.Basic
+import Mathlib.Analysis.Convex.Integral
 import Mathlib.MeasureTheory.Integral.IntervalIntegral.Basic
 
 open MeasureTheory
@@ -8,4 +8,6 @@ theorem hermite_hadamard {a b : ℝ} (hab : a < b) {f : ℝ → ℝ}
     (hf : ConvexOn ℝ (Set.Icc a b) f) (hfc : ContinuousOn f (Set.Icc a b)) :
     f ((a + b) / 2) ≤ (1 / (b - a)) * ∫ x in a..b, f x ∧
     (1 / (b - a)) * ∫ x in a..b, f x ≤ (f a + f b) / 2 := by
-  sorry
+  constructor
+  · sorry
+  · sorry
